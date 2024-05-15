78ccdb4f9d6dfbf83ef50eef1957926dd5ff5759
Ruchitas123 <137498837+Ruchitas123@users.noreply.github.com>
2024-05-15 14:45:54 +0530

    Update util.js

--- blocks/form/util.js
+++ blocks/form/util.js
@@ -237,6 +237,7 @@ export function checkValidation(fieldElement) {
     removeInvalidMsg(fieldElement);
     return;
   }
+
   const message = translate(getValidationMessage(fieldElement, wrapper));
   updateOrCreateInvalidMsg(fieldElement, message);
 }
